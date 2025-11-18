1a6bd8014c1781dd4a0856bad0e655bf2c52408e
Niki Ramandika <nikiramandika@icloud.com>
2025-11-18 20:32:27 +0700

    Add DEAD_DISPATCHER import to departemen_controller.py

--- departemen_controller.py
+++ departemen_controller.py
@@ -2,7 +2,7 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, arp, ipv4, icmp
