5cc871d62de3d73f7cdc846a8339038160d1fea2
Niki Ramandika <nikiramandika@icloud.com>
2025-11-18 20:55:48 +0700

    Update IP subnet mask for department hosts to /16 for consistency

--- departemen_topology.py
+++ departemen_topology.py
@@ -2,35 +2,32 @@ from mininet.topo import Topo
 
 class DeptTopo(Topo):
     def build(self):
-        # Membuat 3 Switch
-        s1 = self.addSwitch('s1') # Dept A Switch
-        s2 = self.addSwitch('s2') # Dept B Switch
-        s3 = self.addSwitch('s3') # Dept C Switch
+        s1 = self.addSwitch('s1') # Switch Dept A
+        s2 = self.addSwitch('s2') # Switch Dept B
+        s3 = self.addSwitch('s3') # Switch Dept C
 
-        # Membuat Host untuk Dept A (IP 10.0.1.x)
-        h1 = self.addHost('h1', ip='10.0.1.1/24')
-        h2 = self.addHost('h2', ip='10.0.1.2/24')
+        # Perhatikan: IP menggunakan /16, bukan /24
+        # Dept A
+        h1 = self.addHost('h1', ip='10.0.1.1/16')
+        h2 = self.addHost('h2', ip='10.0.1.2/16')
         
-        # Membuat Host untuk Dept B (IP 10.0.2.x)
-        h3 = self.addHost('h3', ip='10.0.2.1/24')
-        h4 = self.addHost('h4', ip='10.0.2.2/24')
+        # Dept B
+        h3 = self.addHost('h3', ip='10.0.2.1/16')
+        h4 = self.addHost('h4', ip='10.0.2.2/16')
 
-        # Membuat Host untuk Dept C (IP 10.0.3.x)
-        h5 = self.addHost('h5', ip='10.0.3.1/24')
-        h6 = self.addHost('h6', ip='10.0.3.2/24')
+        # Dept C
+        h5 = self.addHost('h5', ip='10.0.3.1/16')
+        h6 = self.addHost('h6', ip='10.0.3.2/16')
 
-        # Menghubungkan Host ke Switch masing-masing
+        # Link Host ke Switch
         self.addLink(h1, s1)
         self.addLink(h2, s1)
-        
         self.addLink(h3, s2)
         self.addLink(h4, s2)
-        
         self.addLink(h5, s3)
         self.addLink(h6, s3)
 
-        # Menghubungkan Switch (Trunk links)
-        # S1 <-> S2 <-> S3
+        # Link Antar Switch
         self.addLink(s1, s2)
         self.addLink(s2, s3)
 
